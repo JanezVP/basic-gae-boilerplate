468e407b8e33ed30ad079c27291ffb3d5bb293ac
JanezVP <janez.voler@gmail.com>
2016-11-17 13:09:52 +0100

    css je ok

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        return self.response.write('Hello SmartNinja!')
-
+        stran = open('Index.html')
+        return self.response.write(stran.read())
+"""
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+            return self.response.write('Hello SmartNinja!')
+            return self.response.write('Hello SmartNinja!')
+"""
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
 ], debug=True)
